45ecfb29efa8444b8cbd81d47241a4c07d052da1
epevel <evan_eveland@nycap.rr.com>
2016-09-27 09:32:46 -0400

    Initial checkin of VB Spiro

---
+++ VBSpiro/VBSpiro/MainWindow.xaml.vb
@@ -0,0 +1,271 @@
+﻿Partial Public Class MainWindow
+    Inherits Window
+
+    Private fixedCircle As Ellipse
+    Private movingCircle As Ellipse
+    Private penLine As Polyline
+    Private gridLines As List(Of Polyline)
+    Private p1 As Polyline
+
+    Private showCircles As Boolean
+    Private showGrid As Boolean
+
+    Private type As Trochoid.TrochoidType
+
+    Private doneLoading As Boolean
+    Public Property pl As Polyline
+    ' TODO:
+    ' arbitrary number of Polyline objects
+    ' ability to add new ones
+
+    Public Sub New()
+        InitializeComponent()
+        doneLoading = False
+        showCircles = True
+        showGrid = True
+        type = Trochoid.TrochoidType.Hypotrochoid
+
+        createCirclesAndLine()
+    End Sub
+
+    Private Sub Window_ContentRendered(sender As Object, e As EventArgs)
+        doneLoading = True
+        createGridLines()
+        setCircleAndLinePositions()
+        showHideCirclesAndLine()
+        showHideGrid()
+    End Sub
+
+    Private Sub createGridLines()
+        Dim canvasCenterX As Integer = CType(Convert.ToInt32(renderCanvas.ActualWidth) / 2, Integer)
+        Dim canvasCenterY As Integer = CType(Convert.ToInt32(renderCanvas.ActualHeight) / 2, Integer)
+
+        gridLines = New List(Of Polyline)()
+        ' horiz line
+        gridLines.Add(New Polyline() With {
+            .Stroke = New SolidColorBrush(Colors.Gray),
+            .StrokeThickness = 1,
+            .Points = New PointCollection() From {
+                New Point(canvasCenterX, 0),
+                New Point(canvasCenterX, renderCanvas.ActualHeight)
+            }
+        })
+        ' vert line
+        gridLines.Add(New Polyline() With {
+            .Stroke = New SolidColorBrush(Colors.Gray),
+            .StrokeThickness = 1,
+            .Points = New PointCollection() From {
+                New Point(0, canvasCenterY),
+                New Point(renderCanvas.ActualWidth, canvasCenterY)
+            }
+        })
+
+        For Each p As Polyline In gridLines
+            renderCanvas.Children.Add(p)
+        Next
+
+    End Sub
+
+
+    Private Sub createCirclesAndLine()
+        fixedCircle = New Ellipse() With {
+            .Uid = "fixedCircle",
+            .Stroke = New SolidColorBrush(Colors.DarkBlue),
+            .StrokeThickness = 1
+        }
+        movingCircle = New Ellipse() With {
+            .Uid = "movingCircle",
+            .Stroke = New SolidColorBrush(Colors.DarkGreen),
+            .StrokeThickness = 1
+        }
+        penLine = New Polyline() With {
+            .Uid = "penLine",
+            .Stroke = New SolidColorBrush(Colors.DarkRed),
+            .StrokeThickness = 1
+        }
+        Me.renderCanvas.Children.Add(fixedCircle)
+        Me.renderCanvas.Children.Add(movingCircle)
+        Me.renderCanvas.Children.Add(penLine)
+    End Sub
+
+    Private Sub setCircleAndLinePositions()
+        If doneLoading Then
+            Dim canvasCenterX As Integer = CType(Convert.ToInt32(renderCanvas.ActualWidth) / 2, Integer)
+            Dim canvasCenterY As Integer = CType(Convert.ToInt32(renderCanvas.ActualHeight) / 2, Integer)
+
+            Dim fixedR As Integer = Convert.ToInt32(Me.FixedRadius.Value)
+            Dim movingR As Integer = Convert.ToInt32(Me.MovingRadius.Value)
+            Dim penD As Integer = Convert.ToInt32(Me.PenDistance.Value)
+
+            ' set top left position of the fixed circle
+            Dim fixedCircleTop As Integer = canvasCenterY - fixedR
+            Dim fixedCircleLeft As Integer = canvasCenterX - fixedR
+            Dim movingCircleTop As Integer
+            Dim movingCircleLeft As Integer
+            Dim penLineY As Integer = canvasCenterY
+            Dim penLineX1 As Integer, penLineX2 As Integer
+
+
+            If type = Trochoid.TrochoidType.Hypotrochoid Then
+                ' set top left position of the moving circle and line
+                movingCircleTop = canvasCenterY - movingR
+                movingCircleLeft = canvasCenterX + fixedR - 2 * movingR
+                penLineX1 = canvasCenterX + fixedR - movingR
+                penLineX2 = penLineX1 + penD
+            Else
+                ' set top left position of the moving circle and line                    
+                movingCircleTop = canvasCenterY - movingR
+                movingCircleLeft = canvasCenterX + fixedR
+                penLineX1 = canvasCenterX + fixedR + movingR
+                penLineX2 = penLineX1 + penD
+            End If
+
+            ' now update the ellipse and line objects...
+            If fixedCircle IsNot Nothing Then
+                fixedCircle.Width = 2 * fixedR
+                fixedCircle.Height = 2 * fixedR
+                Canvas.SetTop(fixedCircle, fixedCircleTop)
+                Canvas.SetLeft(fixedCircle, fixedCircleLeft)
+            End If
+            If movingCircle IsNot Nothing Then
+                movingCircle.Width = 2 * movingR
+                movingCircle.Height = 2 * movingR
+                Canvas.SetTop(movingCircle, movingCircleTop)
+                Canvas.SetLeft(movingCircle, movingCircleLeft)
+            End If
+            If penLine IsNot Nothing Then
+                penLine.Points = New PointCollection()
+                penLine.Points.Add(New Point(penLineX1, penLineY))
+                penLine.Points.Add(New Point(penLineX2, penLineY))
+            End If
+        End If
+    End Sub
+    Private Sub showHideGrid()
+        If doneLoading Then
+            If showGrid Then
+                For Each p As Polyline In gridLines
+                    p.Visibility = Visibility.Visible
+                Next
+            Else
+                For Each p As Polyline In gridLines
+                    p.Visibility = Visibility.Hidden
+                Next
+            End If
+        End If
+    End Sub
+
+    Private Sub showHideCirclesAndLine()
+        If doneLoading Then
+            If showCircles Then
+                fixedCircle.Visibility = Visibility.Visible
+                movingCircle.Visibility = Visibility.Visible
+
+
+                penLine.Visibility = Visibility.Visible
+            Else
+                fixedCircle.Visibility = Visibility.Hidden
+                movingCircle.Visibility = Visibility.Hidden
+                penLine.Visibility = Visibility.Hidden
+            End If
+        End If
+    End Sub
+
+    Private Sub Draw_Click(sender As Object, e As RoutedEventArgs)
+        Dim centerX As Integer = CType(Convert.ToInt32(renderCanvas.ActualWidth) / 2, Integer)
+        Dim centerY As Integer = CType(Convert.ToInt32(renderCanvas.ActualHeight) / 2, Integer)
+
+
+        Dim fixedR As Integer = Convert.ToInt32(Me.FixedRadius.Value)
+        Dim movingR As Integer = Convert.ToInt32(Me.MovingRadius.Value)
+        Dim penD As Integer = Convert.ToInt32(Me.PenDistance.Value)
+
+        ' get color...
+        Dim c As Color = Colors.Black
+        If Me.PenColor.SelectedColor.HasValue Then
+            c = Me.PenColor.SelectedColor.Value
+        End If
+
+        ' now get the points
+        Dim p As List(Of Point)
+        p = Trochoid.GetPoints(fixedR, movingR, penD, centerX, centerY, type)
+
+        ' create the polyline
+        Me.pl = New Polyline() With {
+            .Uid = "PolyLine",
+            .StrokeThickness = Me.PenThickness.Value,
+            .Stroke = New SolidColorBrush(c),
+            .Points = New PointCollection(p)
+        }
+        Me.renderCanvas.Children.Add(pl)
+
+    End Sub
+
+
+    Private Sub Clear_Click(sender As Object, e As RoutedEventArgs)
+        removeItemsFromCanvasByUid("PolyLine")
+
+        showHideCirclesAndLine()
+        showHideGrid()
+    End Sub
+
+    Private Sub removeItemsFromCanvasByUid(uid As String)
+        Dim removeObjects As New List(Of UIElement)()
+        For Each u As UIElement In renderCanvas.Children
+
+            If u.Uid.StartsWith(uid) Then
+                removeObjects.Add(u)
+            End If
+        Next
+        For Each u As UIElement In removeObjects
+            renderCanvas.Children.Remove(u)
+        Next
+    End Sub
+
+    Private Sub FixedRadius_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        setCircleAndLinePositions()
+    End Sub
+
+    Private Sub MovingRadius_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        setCircleAndLinePositions()
+    End Sub
+
+    Private Sub PenDistance_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        setCircleAndLinePositions()
+    End Sub
+
+    Private Sub Hypotrochoid_Checked(sender As Object, e As RoutedEventArgs)
+        type = Trochoid.TrochoidType.Hypotrochoid
+        setCircleAndLinePositions()
+        showHideCirclesAndLine()
+
+
+    End Sub
+
+    Private Sub Epitrochoid_Checked(sender As Object, e As RoutedEventArgs)
+        type = Trochoid.TrochoidType.Epitrochoid
+        setCircleAndLinePositions()
+        showHideCirclesAndLine()
+
+    End Sub
+
+    Private Sub hideCircles_Checked(sender As Object, e As RoutedEventArgs)
+        showCircles = False
+        showHideCirclesAndLine()
+    End Sub
+
+    Private Sub hideCircles_Unchecked(sender As Object, e As RoutedEventArgs)
+        showCircles = True
+        showHideCirclesAndLine()
+
+    End Sub
+
+    Private Sub hideGrid_Checked(sender As Object, e As RoutedEventArgs)
+        showGrid = False
+        showHideGrid()
+    End Sub
+
+    Private Sub hideGrid_Unchecked(sender As Object, e As RoutedEventArgs)
+        showGrid = True
+        showHideGrid()
+    End Sub
+End Class
---
+++ VBSpiro/VBSpiro/Trochoid.vb
@@ -0,0 +1,119 @@
+﻿
+Public Class Trochoid
+    ''' <summary>
+    ''' The type of trochoid; 
+    ''' Hypotrochoid: circle moves inside a fixed circle
+    ''' Epitrochoid: circle moves outside a fixed circle
+    ''' </summary>
+    Public Enum TrochoidType
+        Hypotrochoid
+        Epitrochoid
+    End Enum
+
+    ''' <summary>
+    ''' Parametric equations for X of a Trochoid.
+    ''' </summary>
+    ''' <param name="t">Variable t.</param>
+    ''' <param name="fR">Fixed radius.</param>
+    ''' <param name="mR">Moving Radius.</param>
+    ''' <param name="d">Pen distance from center of moving circle.</param>
+    ''' <param name="type">One of Hypotrochoid or Epitrochoid.</param>
+    ''' <returns>The X coordinate with relation to t.</returns>
+    ''' <remarks>
+    ''' https://en.wikipedia.org/wiki/Hypotrochoid
+    ''' https://en.wikipedia.org/wiki/Epitrochoid
+    ''' </remarks>
+    Private Shared Function getX(t As Double, fR As Double, mR As Double, d As Double, type As TrochoidType) As Double
+        If type = TrochoidType.Hypotrochoid Then
+            Return (fR - mR) * Math.Cos(t) + d * Math.Cos((fR - mR) / mR * t)
+        Else
+            Return (fR + mR) * Math.Cos(t) - d * Math.Cos((fR + mR) / mR * t)
+        End If
+    End Function
+
+    ''' <summary>
+    ''' Parametric equations for Y of a Trochoid.
+    ''' </summary>
+    ''' <param name="t">Variable t.</param>
+    ''' <param name="fR">Fixed radius.</param>
+    ''' <param name="mR">Moving Radius.</param>
+    ''' <param name="d">Pen distance from center of moving circle.</param>
+    ''' <param name="type">One of Hypotrochoid or Epitrochoid.</param>
+    ''' <returns>The Y coordinate with relation to t.</returns>
+    ''' <remarks>
+    ''' https://en.wikipedia.org/wiki/Hypotrochoid
+    ''' https://en.wikipedia.org/wiki/Epitrochoid
+    ''' </remarks>
+    Private Shared Function getY(t As Double, fR As Double, mR As Double, d As Double, type As TrochoidType) As Double
+        If type = TrochoidType.Hypotrochoid Then
+            Return (fR - mR) * Math.Sin(t) - d * Math.Sin((fR - mR) / mR * t)
+        Else
+            Return (fR + mR) * Math.Sin(t) - d * Math.Sin((fR + mR) / mR * t)
+        End If
+    End Function
+
+    ''' <summary>
+    ''' Recursive GCD function.
+    ''' </summary>
+    ''' <param name="x">1st int</param>
+    ''' <param name="y">2nd int</param>
+    ''' <returns>The greatest common divisor for the two integers x and y.</returns>
+    Private Shared Function greatestCommonDivisor(x As Integer, y As Integer) As Integer
+        If x Mod y = 0 Then
+            Return y
+        Else
+            Return greatestCommonDivisor(y, x Mod y)
+        End If
+
+    End Function
+
+    Public Shared Function GetPoints(fixedRadius As Integer, movingRadius As Integer, penLength As Integer, centerX As Integer, centerY As Integer, type As TrochoidType) As List(Of Point)
+        Return GetPoints(fixedRadius, movingRadius, penLength, 360, centerX, centerY, type)
+    End Function
+
+    ''' <summary>
+    ''' Given a fixed radius, a moving radius, a pen length and a type find the points that define the coordinates of a trochoid with its center at the coordinates provided.
+    ''' </summary>
+    ''' <param name="fixedRadius">The fixed radius.</param>
+    ''' <param name="movingRadius">The moving radius.</param>
+    ''' <param name="penLength">The distance from the center of the moving circle the pen sits at.</param>
+    ''' <param name="pointsPerRevolution">How many points to define per revolution; higher == smoother curve.</param>
+    ''' <param name="centerX">The x coordinate of the point the trochoid should be centered on.</param>
+    ''' <param name="centerY">The y coordinate of the point the trochoid should be centered on.</param>
+    ''' <param name="type">One of Hypotrochoid or Epitrochoid.</param>
+    ''' <returns>A list of points that represent the coordinates of the trochoid.</returns>
+    Public Shared Function GetPoints(fixedRadius As Integer, movingRadius As Integer, penLength As Integer,
+                                     pointsPerRevolution As Integer, centerX As Integer, centerY As Integer,
+                                     type As TrochoidType) As List(Of Point)
+        Dim retVal As New List(Of Point)()
+        If fixedRadius <> 0 AndAlso movingRadius <> 0 Then
+            Dim gcd As Integer = greatestCommonDivisor(fixedRadius, movingRadius)
+
+            ' starting variable
+            Dim t As Double = 0
+            ' increment; more PointsPerRevolution == smaller increment == smoother curve
+            Dim increment As Double = Math.PI / pointsPerRevolution
+
+            ' the limit of the variable T;  the circumference of the moving circle / GCD of both circles
+            Dim maxT As Double = (2 * Math.PI) * movingRadius / gcd
+
+            ' get the starting point
+            Dim x As Double = centerX + Trochoid.getX(t, fixedRadius, movingRadius, penLength, type)
+            Dim y As Double = centerY + Trochoid.getY(t, fixedRadius, movingRadius, penLength, type)
+            ' add 1st point
+            retVal.Add(New Point(x, y))
+
+            ' get the values of x and y as t goes from 0 to maxT
+            While t <= maxT
+                t += increment
+                x = centerX + Trochoid.getX(t, fixedRadius, movingRadius, penLength, type)
+                y = centerY + Trochoid.getY(t, fixedRadius, movingRadius, penLength, type)
+                retVal.Add(New Point(x, y))
+            End While
+        End If
+        Return retVal
+
+    End Function
+
+End Class
+
